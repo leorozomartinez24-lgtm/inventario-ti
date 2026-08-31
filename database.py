96674fd1365e42f603f54cb17e317b6f73f002dd
leorozomartinez24-lgtm <leorozomartinez24@gmail.com>
2026-08-30 21:10:23 -0500

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,14 @@ import streamlit as st
 
 def get_connection():
     try:
-        conn = psycopg2.connect(st.secrets["DATABASE_URL"])
+        conn = psycopg2.connect(
+            host=st.secrets["database"]["host"],
+            database=st.secrets["database"]["database"],
+            user=st.secrets["database"]["user"],
+            password=st.secrets["database"]["password"],
+            port=st.secrets["database"]["port"],
+            sslmode=st.secrets["database"]["sslmode"]
+        )
         return conn
     except Exception as e:
         st.error(f"Error conectando a la base de datos: {e}")
